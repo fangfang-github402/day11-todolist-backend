357d70ef04147defd5ff7cdde3e4c524f7b29486
OOCLDM\FANGCL <FANGCL@OOCL.com>
2024-12-02 21:09:02 +0800

    feat:return 201 when create success

--- src/main/java/com/oocl/todoList/controller/TodoController.java
+++ src/main/java/com/oocl/todoList/controller/TodoController.java
@@ -2,6 +2,8 @@ package com.oocl.todoList.controller;
 
 import com.oocl.todoList.model.Todo;
 import com.oocl.todoList.service.TodoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,8 +23,9 @@ public class TodoController {
     }
 
     @PostMapping
-    public Todo addTodoItem(@RequestBody Todo todoItem) {
-        return todoService.addTodoItem(todoItem);
+    public ResponseEntity<Todo> addTodoItem(@RequestBody Todo todoItem) {
+        Todo createdTodo = todoService.addTodoItem(todoItem);
+        return new ResponseEntity<>(createdTodo, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
